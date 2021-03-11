01f94b6d210278c14c473257817def09c8fa70c9
SerjQazi <sirajum.qazi@gmail.com>
2021-03-10 22:21:51 -0500

    mobile testing, Safari webkit issue (backface-visibility)

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -3,7 +3,7 @@ import { IoBeerOutline } from 'react-icons/io5';
 function WelcomePage () {
   return (
     <div className='welcomePage'>
-      <p className='welcomeTxt'>Please select a State to see your list of Breweries</p>
+      <p className='welcomeTxt'>T2Please select a State to see your list of Breweries</p>
       <div className='welcomeIcon'>
         <li className='iconOne'><IoBeerOutline /></li>
         <li className='iconTwo'><IoBeerOutline /></li>
